6b2181cd7e0012ce582ad0aaf23e6d1d804018a4
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-08 18:34:03 -0300

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,11 @@
+function twoSum(numbers, target) {
+  for (let num1_i = 0; num1_i < numbers.length; num1_i++) {
+    const num1 = numbers[num1_i]
+    for (let num2_i = 0; num2_i < numbers.length; num2_i++) {
+      const num2 = numbers[num2_i]
+      if (num2_i !== num1_i && num1 + num2 === target) {
+        return [num1_i, num2_i]
+      }
+    }
+  }
+}
