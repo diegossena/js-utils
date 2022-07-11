d0993a24f1dbc834bb480fe2eaff66433e3029c3
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-11 09:52:40 -0300

    Create arrayJoin.js

---
+++ arrayJoin.js
@@ -0,0 +1,9 @@
+/**
+ * @author Diego Sena <diego.souza.sena10@gmail.com>
+ * @template T
+ * @param {T[]} array
+ * @param {T} separator
+ * @return {T[]}
+ */
+function arrayJoin(array, separator) {
+  return
