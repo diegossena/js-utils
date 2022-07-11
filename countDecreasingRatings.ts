ecb4bc04b57cb9f503f11f590e1bbaab8a840471
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-11 11:36:05 -0300

    Update and rename countDecreasingRatings.js to countDecreasingRatings.ts

--- countDecreasingRatings.ts
+++ countDecreasingRatings.ts
@@ -1,18 +1,19 @@
 /**
  * @param {number[]} ratings
+ * @return {number}
  */
- function countDecreasingRatings(ratings) {
+function countDecreasingRatings(ratings: number[]): number {
   if (ratings.length <= 1 || ratings.length > 10 ** 5)
     throw Error('Invalid ratings length')
   if (ratings.filter(rating => rating > 0 && rating <= 10 ** 9).length !== ratings.length)
     throw Error('Invalid rating number')
-    
+
   let count = ratings.length
 
   for (let group_length = 2; group_length < ratings.length; group_length++) {
 
     for (let rating_i = 0; rating_i <= ratings.length - group_length; rating_i++) {
-      
+
       const group = ratings.slice(rating_i, rating_i + group_length)
       let isValid = true
 
