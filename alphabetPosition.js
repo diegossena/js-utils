c579e0eb88d6697220f568aa01f266634333658c
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-11 11:02:22 -0300

    Update and rename alphabetPosition.js to alphabetPosition.ts

--- alphabetPosition.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author Diego Sena <diego.souza.sena10@gmail.com>
- * @param {string} letter
- */
-function alphabetPosition(letter) {
-  return letter.toLowerCase().charCodeAt() - 96
-}
---
+++ alphabetPosition.ts
@@ -0,0 +1,6 @@
+/**
+ * @author Diego Sena <diego.souza.sena10@gmail.com>
+ */
+function alphabetPosition(letter: string) {
+  return letter.toLowerCase().charCodeAt(0) - 96
+}
