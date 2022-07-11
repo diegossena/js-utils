0ba9e31f5f79ab792d8350e2569ff08bfeb1e07d
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-11 09:30:03 -0300

    Update anagram-generator.js

--- anagram-generator.js
+++ anagram-generator.js
@@ -1,7 +1,12 @@
-const usedChars = []
-const permArr = []
-
-function permute(input = ['']) {
+/**
+ * @author Diego Sena <diego.souza.sena10@gmail.com>
+ * @param {Object} opt
+ * @param {string[]} opt.input
+ * @param {string[]} opt.usedChars
+ * @param {string[]} opt.permutes
+ */
+function permute(opt) {
+  const { input, permutes, usedChars } = opt
   // Loop through every letter
   for (let i = 0; i < input.length; i++) {
     // Splice the character at the index we need
@@ -10,17 +15,20 @@ function permute(input = ['']) {
     usedChars.push(ch)
     // We've run out of characters, push this combination into storage
     if (input.length === 0)
-      permArr.push(usedChars.join(''))
+      permutes.push(usedChars.join(''))
     // Recursively run the input without the characters we've removed
-    permute(input)
+    permute(opt)
     // Inject and shift our stored letter at the appropriate index, this is how arrange for all possible combinations
     input.splice(i, 0, ch)
     // Remove the last used character as we've exhausted its potential
     usedChars.pop()
   }
-  return permArr
+  return permutes
 }
-
-console.log(
-  permute(Array.from('abcde'))
-)
+/**
+ * @author Diego Sena <diego.souza.sena10@gmail.com>
+ * @param {string} str
+ */
+function anagrams(str) {
+  return permute({ input: Array.from(str), permutes: [], usedChars: [] })
+}
