d114aee29c5d37ccaf20bfc678573ab42250f951
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-11 11:24:12 -0300

    Update alphabetPosition.ts

--- alphabetPosition.ts
+++ alphabetPosition.ts
@@ -1,6 +1,8 @@
 /**
  * @author Diego Sena <diego.souza.sena10@gmail.com>
+ * @param {string} letter
+ * @return {number}
  */
-export default function alphabetPosition(letter: string) {
+export default function alphabetPosition(letter: string): number {
   return letter.toLowerCase().charCodeAt(0) - 96
 }
