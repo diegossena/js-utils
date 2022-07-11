2b458deeb34af7d2ae66f41ff9998639c981582c
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-11 11:23:29 -0300

    Update and rename arrayComponentJoin.js to arrayComponentJoin.ts

--- arrayComponentJoin.ts
+++ arrayComponentJoin.ts
@@ -1,3 +1,4 @@
+/// <reference types="react" />
 /**
  * @author Diego Sena <diego.souza.sena10@gmail.com>
  * @template T
@@ -6,7 +7,11 @@
  * @param {(element: T, key: number) => JSX.Element} elementMap
  * @return {JSX.Element[]}
  */
-function arrayComponentJoin(array, separatorMap, elementMap) {
+export default function arrayComponentJoin<T>(
+  array: T[],
+  separatorMap: (key: number) => JSX.Element,
+  elementMap: (element: T, key: number) => JSX.Element
+): JSX.Element[] {
   return array.length !== 0
     ? Array.from(
       Array(array.length * 2 - 1),
