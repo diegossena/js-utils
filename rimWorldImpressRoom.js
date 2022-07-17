05b4f77228161f7c36497616a7a551f74020d699
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-17 00:55:00 -0300

    Update rimWorldImpressRoom.js

--- rimWorldImpressRoom.js
+++ rimWorldImpressRoom.js
@@ -1,21 +1,32 @@
-
+function space(tiles=0) { return tiles*1.4 }
 function rimWorldImpressRoom(
   wealth = 0,
   space = 0,
   beauty = 0,
   cleanliness = 0,
 ) {
-  const Wb = wealth / 1500
-  const Bb = beauty / 3
-  const Sb = space / 125
-  const Cb = 1 + (cleanliness / 2.5)
+  const ceil = v => parseFloat(Math.ceil(v + 'e2') + 'e-2')
+  const ln = v => ceil(Math.log(v))
+  const Wb = ceil(wealth / 1500)
+  const Bb = ceil(beauty / 3)
+  const Sb = ceil(space / 125)
+  const Cb = ceil(1 + (cleanliness / 2.5))
   const [Wm, Bm, Sm, Cm] = [Wb, Bb, Sb, Cb]
     .map(b => (
       b > 1
-        ? 1 + Math.log(b)
+        ? 1 + ln(b)
         : b < -1
-        ? (1 + Math.log(-b)) * -1
-        : 0
+        ? (1 + ln(-b)) * -1
+        : b
     ))
-  return (65 * (Wm + Bm + Sm + Cm) / 4) + (35 * Math.min(Wm, Bm, Sm, Cm))
+  const I = (65 * (Wm + Bm + Sm + Cm) / 4) + (35 * Math.min(Wm, Bm, Sm, Cm))
+  const S = 500  * Sm
+  return {
+    Wm, Bm, Sm, Cm,
+    Impressive: (
+      I > S 
+        ? 0.25 * I + 0.75 * S
+        : I
+    )
+  }
 }
