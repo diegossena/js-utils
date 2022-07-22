9b8c08a4140b9154f2de62c94b9a444f972e8ccc
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-22 15:25:03 -0300

    Update arrayGroupBy.ts

--- arrayGroupBy.ts
+++ arrayGroupBy.ts
@@ -1,3 +1,17 @@
+/**
+ * ```js
+ * arrayGroupBy([6.5, 4.12, 6.8, 5.4], Math.floor): {
+ *  '4': [ 4.12 ],
+ *  '5': [ 5.4 ],
+ *  '6': [ 6.5, 6.8 ]
+ * }
+ * arrayGroupBy(['one', 'two', 'three', 'four'], 'length'): {
+ *  '3': [ 'one', 'two' ],
+ *  '4': [ 'four' ],
+ *  '5': [ 'three' ]
+ * }
+ * ```
+ */
 export function arrayGroupBy<T extends {}, K extends keyof T>(
   collection: T[],
   iteratee: K | ((value: Pick<T, K>) => any)
